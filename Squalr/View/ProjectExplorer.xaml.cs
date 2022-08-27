157ffde7987fbfcf49a1fbe66ca779d89da9468a
Zachary Canann <zahcarycanann@gmail.com>
2022-08-27 15:17:32 -0700

    Add proj explorer context menu icons. Add support for shift-click in proj explorer

--- Squalr/View/ProjectExplorer.xaml.cs
+++ Squalr/View/ProjectExplorer.xaml.cs
@@ -1,9 +1,12 @@
 ﻿namespace Squalr.View
 {
+    using Squalr.Engine.Projects.Items;
     using Squalr.Source.ProjectExplorer;
     using Squalr.Source.ProjectExplorer.ProjectItems;
     using Squalr.Source.PropertyViewer;
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
     using System.Windows.Controls;
     using System.Windows.Input;
@@ -35,52 +38,142 @@
                 return;
             }
 
-            List<ProjectItemView> selectedItems = new List<ProjectItemView>();
-
             treeView.SelectedItemChanged += (a, b) =>
             {
-                ProjectItemView treeViewItem = treeView.SelectedItem as ProjectItemView;
-
-                if (treeViewItem == null)
+                if (ProjectExplorerViewModel.GetInstance().SelectedProjectItems == null)
                 {
-                    return;
+                    ProjectExplorerViewModel.GetInstance().SelectedProjectItems = new List<ProjectItemView>();
                 }
 
-                // allow multiple selection
-                // when control key is pressed
-                if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+                bool isShiftSelecting = Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift);
+                bool isControlSelecting = Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl);
+
+                if (isShiftSelecting)
                 {
-                    // suppress selection change notification
-                    // select all selected items
-                    // then restore selection change notifications
-                    var isSelectionChangeActive = IsSelectionChangeActiveProperty.GetValue(treeView, null);
+                    // Suppress selection change notification, select all selected items, then restore selection change notifications
+                    Object? isSelectionChangeActive = IsSelectionChangeActiveProperty.GetValue(treeView, null);
 
                     IsSelectionChangeActiveProperty.SetValue(treeView, true, null);
-                    selectedItems.ForEach(item => item.IsSelected = true);
+                    ProjectExplorer.ShiftSelect(treeView);
 
                     IsSelectionChangeActiveProperty.SetValue(treeView, isSelectionChangeActive, null);
                 }
-                else
+                else if (isControlSelecting)
                 {
-                    // deselect all selected items except the current one
-                    selectedItems.ForEach(item => item.IsSelected = item == treeViewItem);
-                    selectedItems.Clear();
-                }
-
-                if (!selectedItems.Contains(treeViewItem))
-                {
-                    selectedItems.Add(treeViewItem);
+                    ProjectExplorer.ReselectPriorSelectedItems(treeView);
+                    ProjectExplorer.ToggleSelection(treeView);
                 }
                 else
                 {
-                    // deselect if already selected
-                    treeViewItem.IsSelected = false;
-                    selectedItems.Remove(treeViewItem);
+                    ProjectExplorer.NormalSelect(treeView);
+                    ProjectExplorer.ToggleSelection(treeView);
                 }
 
-                PropertyViewerViewModel.GetInstance().SetTargetObjects(selectedItems.ToArray());
-                ProjectExplorerViewModel.GetInstance().SelectedProjectItems = selectedItems;
+                PropertyViewerViewModel.GetInstance().SetTargetObjects(ProjectExplorerViewModel.GetInstance().SelectedProjectItems?.ToArray());
             };
+        }
+
+        private static void ReselectPriorSelectedItems(TreeView treeView)
+        {
+            // Suppress selection change notification, select all selected items, then restore selection change notifications
+            Object? isSelectionChangeActive = IsSelectionChangeActiveProperty.GetValue(treeView, null);
+
+            IsSelectionChangeActiveProperty.SetValue(treeView, true, null);
+            ProjectExplorerViewModel.GetInstance().SelectedProjectItems?.ForEach(item => item.IsSelected = true);
+            
+            IsSelectionChangeActiveProperty.SetValue(treeView, isSelectionChangeActive, null);
+        }
+
+        private static void ShiftSelect(TreeView treeView)
+        {
+            ProjectItemView clickedTreeViewItem = treeView.SelectedItem as ProjectItemView;
+            ProjectItemView selectedItem = ProjectExplorerViewModel.GetInstance().SelectedProjectItems?.FirstOrDefault();
+            DirectoryItemView root = ProjectExplorerViewModel.GetInstance().ProjectRoot?.FirstOrDefault();
+
+            if (root == null || clickedTreeViewItem == null || selectedItem == null)
+            {
+                return;
+            }
+
+            bool isSelecting = (root == selectedItem) || (root == clickedTreeViewItem);
+
+            ProjectExplorerViewModel.GetInstance().SelectedProjectItems?.Clear();
+            ProjectExplorer.SelectRange(root, selectedItem, clickedTreeViewItem, ref isSelecting);
+        }
+
+        private static bool SelectRange(DirectoryItemView currentDirectory, ProjectItemView rangeStart, ProjectItemView rangeEnd, ref bool isSelecting)
+        {
+            bool selectionComplete = false;
+
+            if (currentDirectory.ChildItems == null)
+            {
+                return selectionComplete;
+            }
+
+            foreach (ProjectItem projectItem in currentDirectory.ChildItems)
+            {
+                ProjectItemView projectItemView = projectItem.MappedView as ProjectItemView;
+                DirectoryItemView directoryItemView = projectItemView as DirectoryItemView;
+                bool selectionStarted = false;
+
+                if (!isSelecting)
+                {
+                    isSelecting = (projectItemView == rangeStart) || (projectItemView == rangeEnd);
+                    selectionStarted = true;
+                }
+
+                if (projectItemView != null)
+                {
+                    projectItemView.IsSelected = isSelecting;
+
+                    if (isSelecting)
+                    {
+                        ProjectExplorerViewModel.GetInstance().SelectedProjectItems?.Add(projectItemView);
+                    }
+                }
+
+                if (directoryItemView != null && directoryItemView.IsExpanded)
+                {
+                    selectionComplete = ProjectExplorer.SelectRange(directoryItemView, rangeStart, rangeEnd, ref isSelecting);
+                }
+
+                if (!selectionStarted && isSelecting && ((projectItemView == rangeStart) || (projectItemView == rangeEnd)))
+                {
+                    isSelecting = false;
+                    selectionComplete = true;
+                    break;
+                }
+            }
+
+            return selectionComplete;
+        }
+
+        private static void NormalSelect(TreeView treeView)
+        {
+            // deselect all selected items except the current one
+            ProjectItemView clickedTreeViewItem = treeView.SelectedItem as ProjectItemView;
+            ProjectExplorerViewModel.GetInstance().SelectedProjectItems?.ForEach(item => item.IsSelected = item == clickedTreeViewItem);
+            ProjectExplorerViewModel.GetInstance().SelectedProjectItems?.Clear();
+        }
+
+        private static void ToggleSelection(TreeView treeView)
+        {
+            ProjectItemView clickedTreeViewItem = treeView.SelectedItem as ProjectItemView;
+
+            if (clickedTreeViewItem == null)
+            {
+                return;
+            }
+
+            if (!ProjectExplorerViewModel.GetInstance().SelectedProjectItems?.Contains(clickedTreeViewItem) ?? false)
+            {
+                ProjectExplorerViewModel.GetInstance().SelectedProjectItems?.Add(clickedTreeViewItem);
+            }
+            else
+            {
+                clickedTreeViewItem.IsSelected = false;
+                ProjectExplorerViewModel.GetInstance().SelectedProjectItems?.Remove(clickedTreeViewItem);
+            }
         }
     }
     //// End class
