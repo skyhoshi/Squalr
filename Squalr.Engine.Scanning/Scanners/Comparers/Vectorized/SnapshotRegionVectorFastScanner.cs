57cf7d9b2033262f3e4b068ba9329cbecc8798c0
Zachary Canann <zahcarycanann@gmail.com>
2022-11-30 20:57:21 -0800

    Clear snapshots button binding fix

--- Squalr.Engine.Scanning/Scanners/Comparers/Vectorized/SnapshotRegionVectorFastScanner.cs
+++ Squalr.Engine.Scanning/Scanners/Comparers/Vectorized/SnapshotRegionVectorFastScanner.cs
@@ -32,6 +32,15 @@
         {
             this.Initialize(region : region, constraints: constraints);
 
+            // This algorithm has three stages:
+            // 1) Scan the first vector of memory, which may contain elements we do not care about. ie <x, x, x, x ... y, y, y, y>,
+            //      where x is data outside the snapshot region (but within the readgroup), and y is within the region we are scanning.
+            //      to solve this, we mask out the x values such that these will always be considered false by our scan
+            // 2) Scan the middle parts of. These will all fit perfectly into vectors
+            // 3) Scan the final vector, if it exists. This may spill outside of the snapshot region (but within the readgroup).
+            //      This works exactly like the first scan, but reversed. ie <y, y, y, y, ... x, x, x, x>, where x values are masked to be false.
+            //      Note: This mask may also be applied to the first scan, if it is also the last scan (ie only 1 scan total for this region).
+
             Int32 scanCount = this.Region.Range / Vectors.VectorSize + (this.VectorOverread > 0 ? 1 : 0);
             Vector<Byte> misalignmentMask = this.BuildVectorMisalignmentMask();
             Vector<Byte> overreadMask = this.BuildVectorOverreadMask();
@@ -48,7 +57,7 @@
                 this.EncodeScanResults(ref scanResults);
             }
 
-            // Perform final scan
+            // Perform final scan, applying the overread mask if applicable.
             if (scanCount > 1)
             {
                 scanResults = Vector.BitwiseAnd(overreadMask, this.VectorCompare());
