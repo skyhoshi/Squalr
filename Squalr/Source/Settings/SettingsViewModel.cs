2dff2b4c6efe32cb87c93b92e773d282ba2ce1a8
Zachary Canann <zahcarycanann@gmail.com>
2022-12-02 00:30:24 -0800

    Add missing property changed binding for scan multi-thread option

--- Squalr/Source/Settings/SettingsViewModel.cs
+++ Squalr/Source/Settings/SettingsViewModel.cs
@@ -608,6 +608,7 @@
             set
             {
                 ScanSettings.UseMultiThreadScans = value;
+                this.RaisePropertyChanged(nameof(this.UseMultiThreadScans));
             }
         }
 
